689d948360bb9a430ae4fa4a0e7675e73a28890f
TRUNG TRAN <quoctrun@ualberta.ca>
2020-10-01 19:30:38 -0600

    Added Triangle

---
+++ src/app/src/main/java/com/example/lab4_participation_exercise/Triangle.java
@@ -0,0 +1,9 @@
+package com.example.lab4_participation_exercise;
+
+public class Triangle extends Shape{
+    private int third;
+    public Triangle(int first, int second, int third) {
+        super(first, second);
+        this.third = third;
+    }
+}
