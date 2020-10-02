d7b3f8c94eb22f83f0c5e603a28ed1d313b1cdb8
TRUNG TRAN <quoctrun@ualberta.ca>
2020-10-01 19:37:39 -0600

    Added black color in Shape

--- src/app/src/main/java/com/example/lab4_participation_exercise/Shape.java
+++ src/app/src/main/java/com/example/lab4_participation_exercise/Shape.java
@@ -3,6 +3,7 @@ package com.example.lab4_participation_exercise;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "black";
 
     public Shape(int x, int y) {
         this.x = x;
