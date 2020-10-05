38bd2739e5b8dd30d1a3f6ea6e2f1b41cfcd7108
Meilin-Lyu <mlyu@ualberta.ca>
2020-10-05 12:12:57 -0600

    add string

--- src/app/src/main/java/com/example/lab4_participation_exercise/Shape.java
+++ src/app/src/main/java/com/example/lab4_participation_exercise/Shape.java
@@ -3,7 +3,7 @@ package com.example.lab4_participation_exercise;
 public abstract class Shape {
     private int x;
     private int y;
-
+    String color = "blue";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
